21038a21375889299cf05f711f5a978fcc21519e
Alberto Fernandez <alberto.fernandez.491@gmail.com>
2016-08-18 19:22:23 +0100

    removed old comments from pipe

--- src/page-sidebar/src/tree/tree.search.pipe.ts
+++ src/page-sidebar/src/tree/tree.search.pipe.ts
@@ -1,14 +1,6 @@
 import { Pipe, PipeTransform } from '@angular/core';
 import { DomainTree } from './tree'
-/*
- * Raise the value exponentially
- * Takes an exponent argument that defaults to 1.
- * Usage:
- *   value | exponentialStrength:exponent
- * Example:
- *   {{ 2 |  exponentialStrength:10}}
- *   formats to: 1024
-*/
+
 @Pipe({name: 'tdTreeSearch'})
 export class TreeSearchPipe implements PipeTransform {
   transform(value: DomainTree, query: string): DomainTree {
